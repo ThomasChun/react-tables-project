26542584862dbb1fd1da4e01e86b86e85bc5b164
Thomas Chun <tchun2055@gmail.com>
2019-05-30 13:27:12 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,87 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import ReactTable from 'react-table';
+import 'react-table/react-table.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      posts: []
+    }
+  }
+
+  componentDidMount() {
+    const url = 'https://jsonplaceholder.typicode.com/posts'
+    fetch(url, {
+      method: 'GET'
+    }).then(res => res.json()).then(posts => {
+      this.setState({posts: posts})
+    })
+  }
+
+  deleteRow(id) {
+    const index = this.state.posts.findIndex(post => post.id === id);
+    this.setState({
+      posts: [...this.state.posts.slice(0, index), ...this.state.posts.slice(index+1)]
+    })
+  }
+
+  render() {
+    const columns = [
+      {
+        Header: 'User ID',
+        accessor: 'userId',
+        style: {
+          textAlign: 'center'
+        },
+        width: 100,
+        maxWidth: 100,
+        minWidth: 100,
+      },
+      {
+        Header: 'ID',
+        accessor: 'id',
+        style: {
+          textAlign: 'center',
+        },
+        width: 100,
+        maxWidth: 100,
+        minWidth: 100,
+      },
+      {
+        Header: 'Title',
+        accessor: 'title'
+      },
+      {
+        Header: 'Content',
+        accessor: 'body'
+      },
+      {
+        Header: 'Actions',
+        Cell: props => {
+          return (
+            <button onClick={() => {
+              this.deleteRow(props.original.id);
+            }}>Delete</button>
+          )
+        },
+        sortable: false,
+        filterable: false,
+        width: 100,
+        maxWidth: 100,
+        minWidth: 100,
+      },
+    ]
+    return (
+      <ReactTable
+        columns={columns}
+        data={this.state.posts}
+        filterable
+        defaultPageSize={20}
+      >
+
+      </ReactTable>
+    )
+  }
 }
-
-export default App;
